68b500c6b30a728dc1343704ee3a46d5f6966dc7
Amchik <am4ik1337@gmail.com>
2022-10-29 18:59:50 +0300

    Improve CasinoType, add /self endpoint

--- entity/src/user.rs
+++ entity/src/user.rs
@@ -1,4 +1,8 @@
-use sea_orm::entity::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rand::{thread_rng, Rng};
+use sea_orm::{entity::prelude::*, ActiveValue::NotSet, IntoActiveModel, Set};
+use serde::{ser::SerializeStruct, Serialize};
 
 #[derive(Clone, Debug, DeriveEntityModel)]
 #[sea_orm(table_name = "users")]
@@ -21,8 +25,9 @@ pub struct Model {
     pub casino_type: Option<CasinoType>,
 }
 
-#[derive(Debug, PartialEq, Clone, EnumIter, DeriveActiveEnum)]
+#[derive(Debug, Serialize, PartialEq, Copy, Clone, EnumIter, DeriveActiveEnum)]
 #[sea_orm(rs_type = "i32", db_type = "Integer")]
+#[serde(rename_all = "camelCase")]
 pub enum CasinoType {
     #[sea_orm(num_value = 0)]
     Poor,
@@ -32,3 +37,101 @@ pub enum CasinoType {
 pub enum Relation {}
 
 impl ActiveModelBehavior for ActiveModel {}
+
+impl Model {
+    pub fn casino_breaking_time_left(&self) -> i64 {
+        0
+    }
+
+    pub async fn calculate_casino_progress(self, db: &DatabaseConnection) -> Result<Self, DbErr> {
+        let casino_type = match self.casino_type {
+            Some(x) => x,
+            None => return Ok(self),
+        };
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+        if now < self.casino_breaking_start + casino_type.time_to_break() {
+            return Ok(self);
+        }
+
+        let (balance, xp) = (self.balance, self.xp);
+
+        let mut active = self.into_active_model();
+        {
+            let mut rng = thread_rng();
+
+            active.balance = Set(balance + rng.gen_range(casino_type.money_range()));
+            active.xp = Set(xp + rng.gen_range(casino_type.xp_range()));
+            active.casino_type = NotSet;
+        }
+
+        active.update(db).await
+    }
+}
+
+impl Serialize for Model {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64;
+
+        let mut s = serializer.serialize_struct("Model", 5)?;
+        s.serialize_field("id", &self.id)?;
+        s.serialize_field("xp", &self.xp)?;
+        s.serialize_field("balance", &self.balance)?;
+        s.serialize_field(
+            "casino_breaking_left",
+            &(now - self.casino_type.map(|f| f.time_to_break()).unwrap_or(0)),
+        )?;
+        s.serialize_field("casino_type", &self.casino_type)?;
+
+        s.end()
+    }
+}
+
+pub mod casinotype {
+    use super::CasinoType;
+    use std::ops::Range;
+
+    macro_rules! impl_casinotype {
+        ($($type:ident -> (money: $money_range:expr, time: $time_range:expr, xp: $xp_range:expr)),+) => {
+            pub const fn money_range(&self) -> Range<i32> {
+                match self {
+                    $(
+                        Self::$type => $money_range,
+                    )+
+                }
+            }
+
+            pub const fn xp_range(&self) -> Range<i32> {
+                match self {
+                    $(
+                        Self::$type => $xp_range,
+                    )+
+                }
+            }
+
+            pub const fn time_to_break(&self) -> i64 {
+                match self {
+                    $(
+                        Self::$type => $time_range,
+                    )+
+                }
+            }
+        };
+    }
+
+    impl CasinoType {
+        impl_casinotype! {
+            // TODO: change it
+            Poor -> (money: 500..1200, time: 15, xp: 7..18)
+        }
+    }
+}
--- src/routes/casino.rs
+++ src/routes/casino.rs
@@ -1,8 +1,29 @@
-use rocket::get;
+use lazy_static::__Deref;
+use migration::sea_orm::{
+    ActiveModelBehavior, ActiveModelTrait, ActiveValue, DatabaseConnection, EntityTrait,
+};
+use rocket::{get, State};
 
 use crate::{response::APIResponse, telegramauth::TelegramAuth};
 
 #[get("/self")]
-pub fn get_user(auth: TelegramAuth) -> APIResponse {
-    APIResponse::new(auth.0)
+pub async fn get_user(auth: TelegramAuth, db: &State<DatabaseConnection>) -> APIResponse {
+    let user = auth.get_user().unwrap();
+    let db_user = entity::user::Entity::find_by_id(user.id as i64)
+        .one(db.deref())
+        .await
+        .ok()
+        .flatten();
+
+    let db_user = match db_user {
+        Some(u) => u.calculate_casino_progress(db.deref()).await,
+        None => {
+            let mut active = entity::user::ActiveModel::new();
+            active.id = ActiveValue::Set(user.id as i64);
+
+            active.insert(db.deref()).await
+        }
+    };
+
+    APIResponse::new(db_user.unwrap())
 }
